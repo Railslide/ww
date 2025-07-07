f6563ca57667b5cca04a897616bcfef4fee0da39
Giulia Vergottini <hello@railslide.io>
2025-07-07 20:14:41 +0200

    Handle special cases

--- internal/whichweek.go
+++ internal/whichweek.go
@@ -10,12 +10,19 @@ type WeekDateRange struct {
 }
 
 func CalculateWeek(week, year int) WeekDateRange {
-	janFirst := time.Date(year, time.January, 0, 0, 0, 0, 0, time.UTC) // TODO: change timezone?
-	monday := janFirst.AddDate(0, 0, (1 - int(janFirst.Weekday())))
+	// Find the week of January 1st
+	janFirst := time.Date(year, time.January, 0, 0, 0, 0, 0, time.UTC)
+	monday := janFirst.AddDate(0, 0, 1 - int(janFirst.Weekday()))
 	sunday := janFirst.AddDate(0, 0, 7 - int(janFirst.Weekday()))
 
-	// Amount of days between the first week of the year and the target one
-	days := 7 * (week - 1)
+	// Remove one week if January 1st is already in week one,
+	// so that week one doesn't get counted twice.
+	if _, w := janFirst.ISOWeek(); w == 1 {
+		week = week - 1
+	}
+
+	// number of days between the week of January 1st and the target week
+	days := 7 * week
 	return WeekDateRange{
 		Start: monday.AddDate(0, 0, days),
 		End:   sunday.AddDate(0, 0, days),
